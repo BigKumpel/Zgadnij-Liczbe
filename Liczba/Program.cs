d2e6279dde788b3a1bea18c1a58bd5e7dc0fc7a9
3109925 <3109925@TE-GDA-D-D26F14>
2022-03-11 14:54:53 +0100

    Dodaj pliki projektów.

---
+++ Liczba/Program.cs
@@ -0,0 +1,33 @@
+﻿Random random = new Random();
+bool play = true;
+int min = 1;
+int max = 10;
+int number;
+int guess;
+
+while(play)
+{
+    guess = 0;
+    number = random.Next(min, max );
+
+    while (guess != number)
+    {
+        Console.WriteLine("Odgadnij liczbę od " + min + " - " + max + " : ");
+        guess = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("zgadywana: " + guess);
+
+        if (guess < number)
+        {
+            Console.WriteLine(guess + " jest zamałe");
+        }
+        else if (guess > number)
+        {
+            Console.WriteLine(guess + " jest zaduże");
+        }
+    }
+    Console.WriteLine("Liczba: " + number);
+    Console.WriteLine("Zgadłeś!");
+    break;
+}
+
+Console.ReadKey();
